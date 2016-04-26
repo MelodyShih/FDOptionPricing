9100ed230e1c677ddaa1a34fae5f1a3540b76845
YuHsuan Shih <yoyoshih13@gmail.com>
2016-04-25 22:32:29 -0400

    correct some erro and add left, right, upper bc

--- backwardeuler.py
+++ backwardeuler.py
@@ -15,14 +15,17 @@ import matplotlib.pyplot as plt
 # v --> v(\eta)
 # alpha -- 
 # beta  --
-
 # Range of stock price
-S_max = 
-S_min =
+S_max = 100.0
+S_min = 0.0
+v_max = 
 
-alpha = 
-beta  = 
-
+alpha = 1.0
+beta  = 1.0
+K     = 
+c1    = numpy.arcsinh( (S_max - K)/alpha )
+c2    = numpy.arcsinh( (S_min - K)/alpha )
+d     = numpy.arcsinh( v_max/beta )
 # Set Coefficient 
 # (Citation: https://drive.google.com/drive/u/1/folders/0B97aYusZC-S0NkJ5OURlbnNCZlk)
 # rho   -- instantaneous correlation between the Brownian motions
@@ -32,12 +35,12 @@ beta  =
 # kappa -- mean reversion speed
 # theta -- long run variance 
 
-rho   =
-sigma = 
-r     = 
-q     = 
-kappa = 
-theta =  
+rho   = 1.0
+sigma = 1.0
+r     = 1.0
+q     = 1.0
+kappa = 1.0
+theta = 1.0
 
 # Set grid
 # Domain (t x \xi x \eta) : 
@@ -45,13 +48,39 @@ theta =
 # [0, T] x [0, 1] x [0,1]
 #
 t_final = 
-n = 100
-m = 100
-time_steps = 100
+N = 100
+M = 100
+L = 100
 
-delta_xi  = numpy.linspace(0, 1, n) 
-delta_ita = numpy.linspace(0, 1, m)
-delta_t   = numpy.linspace(0, t_final, time_steps)
+xi  = numpy.linspace(0, 1, N+2) 
+eta = numpy.linspace(0, 1, M+2)
+t   = numpy.linspace(0, t_final, L)
+
+delta_xi  =  xi[1] -  xi[0]
+delta_eta = eta[1] - eta[0]
+delta_t   =   t[1] -   t[0]
 
 # Some function needed because of the change of variables
-function DSDxi = 
+S       = lambda xi: K + numpy.sinh( c1*xi + c2*(1-xi) )
+dSdxi   = lambda xi: numpy.cosh( c1*xi + c2*(1-xi) ) * (c1 - c2)
+d2Sdxi2 = lambda xi: numpy.sinh( c1*xi + c2*(1-xi) ) * (c1 - c2)**2
+dvdeta  = lambda eta: numpy.cosh( d*eta ) * d * beta
+d2vdv2  = lambda eta: numpy.cosh( d*eta ) * d**2 * beta
+
+xi  =  xi[1:-1]
+eta = eta[1:-1]
+# Boundary condition 
+# For left bc i.e. xi[0] 
+_l = 2.0 / ( 1.0 + 0.5 * delta_xi * d2Sdxi2(xi[1]) )
+l_ = - ( dSdxi(xi[1]) - 0.5 * d2Sdxi2(xi[1]) ) / ( dSdxi(xi[1]) + 0.5 * d2Sdxi2(xi[1]) )
+
+# For right bc i.e. xi[-1]
+_r = 2.0 / ( 1.0 - 0.5 * delta_xi * d2Sdxi2(xi[1]) )
+r_ = - ( dSdxi(xi[-1]) + 0.5 * d2Sdxi2(xi[-1]) ) / ( dSdxi(xi[-1]) - 0.5 * d2Sdxi2(xi[-1]) )
+
+# For upper bc i.e eta[-1] (Move to rhs)
+# rhs[i] = -a[i, M] * S(xi[i-1]) + f[i,M] * S(xi[i]) + a[i,j] * S(xi[i+1])
+rhs = numpy.empty(N*M)
+rhs[0:M-1] =  
+
+# For lower bc (requires us to first solve a firstorder two-dimensional PDE)
